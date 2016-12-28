0503f9e747d31c19d65e7e302a4ffb7cb1c0b23e
Eugene Ha <eha@posteo.de>
2016-12-28 14:03:04 -0500

    Updated doc on utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,23 @@
 # Null-default operator
 `%||%` <- purrr::`%||%`
 
+
+#' Deparse a language object as a single string
+#'
+#' @param x Language object.
+#' @return String.
+#' @keywords internal
 #' @export
 deparse_collapse <- function(x) {
   paste(trimws(deparse(x), which = "both"), collapse = "")
 }
 
+#' Enumerate a character vector as a string
+#'
+#' @param x Character vector.
+#' @param many Integer. How many is too many?
+#' @return String.
+#' @keywords internal
 #' @export
 enumerate_many <- function(x, many = 2L) {
   if (length(x) >= many) {
