e11d06a8268e46a8bc97a26e57091ea819db063c
Eugene Ha <eha@posteo.de>
2016-12-28 14:48:34 -0500

    Test templates

---
+++ tests/testthat/test-formulas.R
@@ -0,0 +1,16 @@
+context("Formulas")
+
+test_that("one-side formula produces a global check formula", {})
+
+test_that("string formula produces a global check formula with message", {})
+
+test_that("unnamed checks in checklist formula use auto-generated messages", {})
+
+test_that("named checks in checklist formula use custom messages", {})
+
+test_that("lifted predicates can check argument lists", {
+  # named case
+  # unnamed case
+})
+
+test_that("anonymous predicates are correctly interpreted", {})
---
+++ tests/testthat/test-strictly.R
@@ -0,0 +1,27 @@
+context("Strictly")
+
+test_that("function unchanged if no checks given", {})
+
+test_that("function environment is preserved", {})
+
+test_that("function attributes are preserved", {})
+
+test_that("function signature is preserved", {})
+
+test_that("'...' checks are combined with .checklist", {})
+
+test_that("adding checks preserves existing checks", {})
+
+test_that("checks can be removed", {})
+
+test_that(".warn_message = FALSE always removes missing argument check", {})
+
+test_that(".warn_message = FALSE always adds missing argument check", {})
+
+test_that("nonstrictly() returns original function, modulo class", {})
+
+test_that("error is raised if .f is not a closure", {})
+
+test_that("error is raised if .warning_message is not NULL or logical", {})
+
+test_that("error is raised if checks form invalid checklist", {})
