40ab2ce58093eadba671d03235f7e451c7f11a5f
Eugene Ha <eha@posteo.de>
2016-12-30 20:19:53 -0500

    Test for string formula checks

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -50,7 +50,56 @@ test_that("one-sided formula produces global check", {
   }, 2)
 })
 
-test_that("string formula produces global check with message", {})
+test_that("string formula produces global check with message", {
+  f <- pass_args(alist(x = , y = , z = 0, ... = , u = y - z, v = ))
+  f_num <- strictly(f, "Not numeric" ~ is.numeric)
+  f_pos <- strictly(f_num, "Not positive" ~ {. > 0})
+
+  # Pass
+  out <- f(1, 2, 3, u = 4, v = 5)
+  expect_identical(f_num(1, 2, 3, u = 4, v = 5), out)
+  expect_identical(f_pos(1, 2, 3, u = 4, v = 5), out)
+
+  # Check failure
+  nms <- c("x", "y", "z", "u", "v")
+  arg_list <- lapply(1:3, function(n) {
+    as.list((1:5) * c(rep(-1, n), rep(1, 5 - n))) %>% setNames(nms)
+  })
+  for (i in seq_along(arg_list)) {
+    for (arg in nms[1:i]) {
+      expect_error(purrr::lift(f_pos)(arg_list[[i]]),
+                   sprintf("Not positive: `%s`", arg))
+    }
+    # No other errors
+    expect_equal(purrr::lift(purrr::safely(f_pos))(arg_list[[i]]) %>% {
+      str_count(.$error, "Not positive")
+    }, i)
+  }
+
+  # Error evaluating check because of missing argument
+  expect_equivalent(f(1, 2), list(1, 2))
+  expect_error(f_num(1, 2), "Error evaluating check.*?argument \"v\" is missing")
+  expect_error(f_pos(1, 2), "Error evaluating check.*?argument \"v\" is missing")
+
+  # Error evaluating check because of invalid input types
+  expect_error(f_pos(1, "y", v = 0), "Not numeric: `y`")
+  expect_error(f_pos(1, "y", v = 0), "Not positive: `z`")
+  expect_error(f_pos(1, "y", v = 0), "Not positive: `v`")
+  expect_error(f_pos(1, "y", v = 0),
+               "Error evaluating check.*?is\\.numeric\\(u\\)")
+  expect_error(f_pos(1, "y", v = 0),
+               "Error evaluating check.*?\\(\\~\\{\\. > 0\\}\\)\\)\\(u\\)")
+  # No other errors
+  expect_equal(purrr::safely(f_pos)(1, "y", v = 0) %>% {
+    str_count(.$error, "Not numeric")
+  }, 1)
+  expect_equal(purrr::safely(f_pos)(1, "y", v = 0) %>% {
+    str_count(.$error, "Not positive")
+  }, 2)
+  expect_equal(purrr::safely(f_pos)(1, "y", v = 0) %>% {
+    str_count(.$error, "Error evaluating check")
+  }, 2)
+})
 
 test_that("function value is reproduced when all checks pass", {
   fs <- lapply(args_list, pass_args)
