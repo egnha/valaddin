80275553a31723d6e144ee1d693688eb218bc22a
Eugene Ha <eha@posteo.de>
2017-01-05 11:49:11 -0500

    More descriptive names

--- R/strictly2.R
+++ R/strictly2.R
@@ -1,19 +1,22 @@
-standardize_error <- function(em) {
-  if (is.null(em$error)) {
-    em$value
+#' @include validate.R
+NULL
+
+project_value <- function(.em) {
+  if (is.null(.em$error)) {
+    .em$value
   } else {
-    msg <- if (is.data.frame(em$error)) {
-      enumerate_many(em$error$msg)
+    msg <- if (is.data.frame(.em$error)) {
+      enumerate_many(.em$error$msg)
     } else {
-      em$error
+      .em$error
     }
 
     stop(msg, call. = FALSE)
   }
 }
 
-strictly2_ <- strictly_with(standardize_error)
-safely_    <- strictly_with(identity, fn_type = error_function)
+strictly2_ <- strictly_with(project_value)
+safely_    <- strictly_with(identity, .fn_type = error_function)
 
 checks <- list(
   list("`.f` not an interpreted function" ~ .f) ~
