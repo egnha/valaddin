054619db3c18e7f4adc4a736d86f47d549e75cc9
Eugene Ha <eha@posteo.de>
2017-01-05 11:50:08 -0500

    More consistent argument naming, handling of zero checks

--- R/validate.R
+++ R/validate.R
@@ -1,21 +1,43 @@
 #' @include monads.R
 NULL
 
-assemble <- function(chk, nm, symb, env = lazyeval::f_env(chk)) {
-  p <- lazyeval::f_rhs(chk)
+eval_expr <- function(.expr) {
+  force(.expr)
+
+  function(.env, ...) suppressWarnings(eval(.expr, envir = .env, ...))
+}
+
+caller <- function(.f) {
+  force(.f)
+
+  function(.call) {
+    .call[[1L]] <- .f
+    .call
+  }
+}
+
+with_sig <- function(.f, .sig, .env = environment(.f)) {
+  f <- eval(call("function", .sig, body(.f)))
+  environment(f) <- .env
+
+  f
+}
+
+assemble <- function(.chk, .nm, .symb, .env = lazyeval::f_env(.chk)) {
+  p <- lazyeval::f_rhs(.chk)
   if (is_lambda(p)) {
-    predicate <- lambda(p, env)
+    predicate <- lambda(p, .env)
     p_symb    <- substitute(purrr::as_function(~x), list(x = p))
   } else {
-    predicate <- lazyeval::f_eval_rhs(chk)
+    predicate <- lazyeval::f_eval_rhs(.chk)
     p_symb    <- p
   }
 
-  lhs <- lazyeval::f_eval_lhs(chk)
+  lhs <- lazyeval::f_eval_lhs(.chk)
   q <- if (is.list(lhs)) {
     do.call(lazyeval::f_list, lhs)
   } else {
-    unfurl_args(lhs, nm, symb, env)
+    unfurl_args(lhs, .nm, .symb, .env)
   }
   string <- purrr::map_chr(q, function(.) {
     expr <- substitute(f(x), list(f = p_symb, x = lazyeval::f_rhs(.)))
@@ -24,16 +46,16 @@ assemble <- function(chk, nm, symb, env = lazyeval::f_env(chk)) {
   is_empty <- names(q) == ""
   names(q)[is_empty] <- sprintf("FALSE: %s", string[is_empty])
 
-  purrr::pmap_df(list(q, string, names(q)), function(x, s, msg) {
+  purrr::pmap_df(list(q, string, names(q)), function(x, s, m) {
     dplyr::data_frame(
       expr   = list(as.call(c(predicate, lazyeval::f_rhs(x)))),
       string = s,
-      msg    = msg
+      msg    = m
     )
   })
 }
 
-predval <- dplyr::data_frame(
+pred_vals <- dplyr::data_frame(
   desc = c("NULL", "NA", "void", "not logical", "TRUE", "FALSE"),
   pred = list(
     purrr::is_null,
@@ -45,18 +67,17 @@ predval <- dplyr::data_frame(
   ),
   value = c(rep(NA, 4), TRUE, FALSE)
 )
-predval$desc <- sprintf("Predicate value is %s: %%s", predval$desc)
+pred_vals$desc <- sprintf("Predicate value is %s: %%s", pred_vals$desc)
 
-# .msg_if_false must not contain "%s"
-value_as_msg <- function(em, .expr, .string, .msg_if_false,
-                         .predval = predval) {
-  if (is.null(em$error)) {
-    .predval$desc[length(.predval$desc)] <- .msg_if_false
-    wh <- which_first_true(.predval$pred, em$value)
-    msg   <- sprintf(.predval$desc[wh], .string)
-    value <- .predval$value[wh]
+# .msg must not contain "%s"
+value_as_msg <- function(.em, .expr, .string, .msg, .vals = pred_vals) {
+  if (is.null(.em$error)) {
+    .vals$desc[length(.vals$desc)] <- .msg
+    wh <- which_first_true(.vals$pred, .em$value)
+    msg   <- sprintf(.vals$desc[wh], .string)
+    value <- .vals$value[wh]
   } else {
-    msg   <- sprintf("Error evaluating check `%s`: %s", .string, em$error)
+    msg   <- sprintf("Error evaluating check `%s`: %s", .string, .em$error)
     value <- NA
   }
 
@@ -68,59 +89,48 @@ value_as_msg <- function(em, .expr, .string, .msg_if_false,
   )
 }
 
-eval_check_expr <- function(expr, string, msg, env) {
-  force(env)
+eval_check <- function(.expr, .string, .msg) {
+  force(.expr); force(.string); force(.msg)
 
-  eval_quietly <- function(.) suppressWarnings(eval(., env))
-  res <- error_fmap(eval_quietly)(error_unit(expr))
+  writer_function(
+    function(.env) {
+      eval_quietly <- function(.) suppressWarnings(eval(., .env))
+      res <- error_fmap(eval_quietly)(error_unit(.expr))
+      log <- value_as_msg(res, .expr, .string, .msg)
 
-  writer(value = env, log = value_as_msg(res, expr, string, msg))
-}
-
-checker <- function(expr, string, msg) {
-  force(expr); force(string); force(msg)
-
-  f <- writer_function(
-   function(env) eval_check_expr(expr, string, msg, env)
+      writer(value = .env, log = log)
+    }
   )
-
-  function(wm) bind.writer_monad(wm, f)
 }
 
-pipeline <- function(.x, .fs, ...) {
-  Reduce(function(x, f) f(x, ...), .fs, init = .x)
+checker <- function(.expr, .string, .msg) {
+  f <- eval_check(.expr, .string, .msg)
+
+  function(.wm) bind.writer_monad(.wm, f)
 }
 
-with_sig <- function(.f, sig, env = environment(.f)) {
-  f <- eval(call("function", sig, body(.f)))
-  environment(f) <- env
-
-  f
-}
-
-validate2_ <- function(wm) {
-  errors <- dplyr::bind_rows(wm$log) %>%
-    `[`(is.na(.$value) | .$value != TRUE, )
-
-  x <- if (nrow(errors)) {
-    list(value = NULL, error = errors)
+validate2_ <- function(.wm) {
+  if (is.null(.wm$log)) {
+    x <- list(value = .wm$value, error = NULL)
   } else {
-    list(value = wm$value, error = NULL)
+    errors <- dplyr::bind_rows(.wm$log) %>%
+      `[`(is.na(.$value) | .$value == FALSE, )
+
+    x <- if (nrow(errors)) {
+      list(value = NULL, error = errors)
+    } else {
+      list(value = .wm$value, error = NULL)
+    }
   }
 
   error(x)
 }
 
-call_with <- function(.f, call) {
-  call[[1L]] <- .f
-  call
-}
+warn <- function(.ref_args) {
+  force(.ref_args)
 
-warn_ <- function(ref_args) {
-  force(ref_args)
-
-  function(call) {
-     missing <- setdiff(ref_args, names(call[-1L]))
+  function(.call) {
+     missing <- setdiff(.ref_args, names(.call[-1L]))
 
      if (length(missing)) {
        msg <- missing %>%
@@ -129,7 +139,7 @@ warn_ <- function(ref_args) {
        warning(msg, call. = FALSE)
      }
 
-     invisible(call)
+     invisible(.call)
   }
 }
 
@@ -137,62 +147,50 @@ proto_strictly <- function(.f, ..., .checklist, .warn_missing, .process) {
   force(.process)
 
   chks <- c(list(...), .checklist)
-  sig <- formals(.f)
 
+  if (!is_checklist(chks)) {
+    stop("Invalid argument checks", call. = FALSE)
+  }
+
+  sig <- formals(.f)
   arg <- nomen(sig)
-  calls <- chks %>%
-    lapply(assemble, nm = arg$nm, symb = arg$symb) %>%
-    dplyr::bind_rows()
-  check_inputs <- purrr::pmap(calls, checker)
+  check_inputs <- if (!length(chks)) {
+    NULL
+  } else {
+    calls <- lapply(chks, assemble, .nm = arg$nm, .symb = arg$symb) %>%
+      dplyr::bind_rows()
+    purrr::pmap(unname(calls), checker)
+  }
   validate <- c(writer_unit, check_inputs, validate2_)
+  call_with <- caller(.f)
   maybe_join <- if (is_error_function(.f)) join.error_monad else identity
-  maybe_warn <- if (.warn_missing) warn_(arg$nm[arg$wo_value]) else invisible
-  call_fn_with <- purrr::partial(call_with, .f = .f)
+  maybe_warn <- if (.warn_missing) warn(arg$nm[arg$wo_value]) else invisible
 
   f <- function(...) {
     call <- match.call()
+
     maybe_warn(call)
 
     env <- do.call(lazyeval::lazy_dots, arg$symb) %>%
       lazy_assign(env = new.env(parent = parent.frame()))
-    call_fn <- call_fn_with(call)
-    eval_fn <- function(e) eval(call_fn, e)
+    call_fn <- call_with(call)
+    pipeline <- c(validate, error_fmap(eval_expr(call_fn)), maybe_join)
 
-    .process(pipeline(env, c(validate, error_fmap(eval_fn), maybe_join)))
+    .process(magrittr::freduce(env, pipeline))
   }
 
   with_sig(f, sig)
 }
 
-strictly_with <- function(process, fn_type = NULL) {
-  force(process)
+strictly_with <- function(.process, .fn_type = NULL) {
+  force(.process)
 
-  type <- fn_type %||% identity
+  type <- .fn_type %||% identity
 
   function(.f, ..., .checklist = list(), .warn_missing = FALSE) {
     type(
       proto_strictly(.f, ..., .checklist = .checklist,
-                     .warn_missing = .warn_missing, .process = process)
+                     .warn_missing = .warn_missing, .process = .process)
     )
   }
 }
-
-standardize_error <- function(em) {
-  if (is.null(em$error)) {
-    em$value
-  } else {
-    msg <- if (is.data.frame(em$error)) {
-      enumerate_many(em$error$msg)
-    } else {
-      em$error
-    }
-
-    stop(msg, call. = FALSE)
-  }
-}
-
-#' @export
-strictly2_ <- strictly_with(standardize_error)
-
-#' @export
-safely_ <- strictly_with(identity, fn_type = error_function)
