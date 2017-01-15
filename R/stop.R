e9f40700950ebf7d7d0d5f4feb563c50a6ed22df
Eugene Ha <eha@posteo.de>
2017-01-15 12:30:35 +0100

    No need for custom stop

--- R/stop.R
+++
@@ -1,6 +0,0 @@
-#' @export
-stop_tersely <- function(cond, domain = NULL) {
-  msg <- conditionMessage(cond)
-#  .Internal(.signalCondition(cond, msg, NULL))
-  .Internal(stop(FALSE, .makeMessage(msg, domain = domain)))
-}
