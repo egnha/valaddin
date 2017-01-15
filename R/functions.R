4f38c5730cfda43547094284654408babc7583f5
Eugene Ha <eha@posteo.de>
2017-01-15 17:51:06 +0100

    Added comments

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,8 @@
-# Call a function against a given call signature
+#' Call a function against a given call signature
+#'
+#' @param .f Function.
+#' @return Function of a call (as a language object).
+#' @keywords internal
 call_fn <- function(.f) {
   force(.f)
 
@@ -8,7 +12,13 @@ call_fn <- function(.f) {
   }
 }
 
-# Rewrite the argument signature of a function
+#' Rewrite the argument signature of a function
+#'
+#' @param .f Function.
+#' @param .sig Pairlist.
+#' @param .attrs Named list (of attributes).
+#' @return Function.
+#' @keywords internal
 with_sig <- function(.f, .sig, .attrs) {
   f <- eval(call("function", .sig, body(.f)))
   environment(f) <- environment(.f)
