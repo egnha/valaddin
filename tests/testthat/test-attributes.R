a0d1eb0e85ac9cf57ec1f06ccb3de7e012075645
Eugene Ha <eha@posteo.de>
2017-01-21 16:52:00 +0100

    Test of strict_core()

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -1,6 +1,15 @@
 context("Attributes of strict closures")
 
-test_that("strict_core() extracts original function", {})
+fs <- lapply(args_list, pass_args)
+has_args <- purrr::map_lgl(args_list, ~ length(nomen(.)$nm) > 0L)
+fs_with_args <- fs[has_args]
+
+test_that("strict_core() extracts original function", {
+  for (f in fs_with_args) {
+    f_strict <- strictly(f, ~is.numeric)
+    expect_identical(strict_core(f_strict), f)
+  }
+})
 
 test_that("strict_checks() extracts checks", {})
 
