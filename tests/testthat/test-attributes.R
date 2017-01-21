6a4a0bba909bc547d7037a6dd086f8ad3dbfd53c
Eugene Ha <eha@posteo.de>
2017-01-21 16:49:13 +0100

    Template for attribute tests

---
+++ tests/testthat/test-attributes.R
@@ -0,0 +1,7 @@
+context("Attributes of strict closures")
+
+test_that("strict_core() extracts original function", {})
+
+test_that("strict_checks() extracts checks", {})
+
+test_that("strict_core() extracts ", {})
