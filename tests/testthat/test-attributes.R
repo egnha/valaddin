f9b04d9b60f196b2d9b363eee26499249323ef89
Eugene Ha <eha@posteo.de>
2017-01-22 12:10:27 +0100

    Test of strict_checks()

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -12,6 +12,30 @@ test_that("strict_core() gets original function", {
 })
 
 test_that("strict_checks() gets checks", {
+  f <- function(x, y, ...) NULL
+  is_positive <- function(x) x > 0
+  is_numeric <- purrr::is_scalar_numeric
+  chks <- list(
+    ~ is_numeric,
+    list(~ x, "y not greater than x" ~ y - x) ~ is_positive
+  )
+  f_strict <- strictly(f, .checklist = chks)
+  chks_df <- strict_checks(f_strict)
+
+  # Exactly four checks
+  expect_equal(nrow(chks_df), 4L)
+
+  exprs <- list(
+    "FALSE: is_numeric(x)"  = substitute(f(x), list(f = is_numeric)),
+    "FALSE: is_numeric(y)"  = substitute(f(y), list(f = is_numeric)),
+    "FALSE: is_positive(x)" = substitute(f(x), list(f = is_positive)),
+    "y not greater than x"  = substitute(f(y-x), list(f = is_positive))
+  )
+
+  # Checks in chks are correctly encoded in chks_df
+  for (msg in names(exprs)) {
+    expect_identical(chks_df[chks_df$msg == msg, ]$expr[[1L]], exprs[[msg]])
+  }
 })
 
 test_that("strict_args() gets names of arguments whose absence is checked", {
