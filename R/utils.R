6987b3fc2b92cf9f1c393ee59866f3bd0118c3fb
Eugene Ha <eha@posteo.de>
2017-02-07 07:08:13 +0100

    Predicate for error conditions

--- R/utils.R
+++ R/utils.R
@@ -10,12 +10,13 @@ skip <- function(...) {}
 is_true <- isTRUE
 is_false <- function(x) identical(FALSE, x)
 
-#' Collapse a character vector into an enumerated string
-#'
-#' @param x Character vector.
-#' @param many Integer. How many is too many?
-#' @return String.
-#' @keywords internal
+# An error object inherits from "condition"
+is_error <- function(x) {
+  wh <- inherits(x, c("error", "condition"), which = TRUE)
+  wh[1L] > 0L && wh[2L] > wh[1L]
+}
+
+# Collapse a character vector into an enumerated string
 enumerate_many <- function(x, many = 2L) {
   if (length(x) >= many) {
     paste(
