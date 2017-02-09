33135c3f34b3a0f21a75b20fea3c51ea9ec9d3dd
Eugene Ha <eha@posteo.de>
2017-02-09 22:31:41 +0100

    For API consistency, check maker should ingest a formula

--- R/checker.R
+++ R/checker.R
@@ -1,48 +1,62 @@
 #' @include strictly.R
 NULL
 
-is_unary <- function(x) {
-  is.function(x) && length(nomen(formals(x))$wo_value) == 1L
-}
-is_rhs_lambda <- function(x) {
-  purrr::is_formula(x) && is_onesided(x) && is_lambda(lazyeval::f_rhs(x))
-}
-is_predicate <- function(x) {
-  is_unary(x) || is_rhs_lambda(x)
+is_gbl_check_formula <- function(x) {
+  is_check_formula(x) && purrr::is_scalar_character(lazyeval::f_eval_lhs(x))
 }
 
-chk_mc <- list(
-  list("`p` must be a function or lambda expression (see ?make_check)" ~ p) ~
-    is_predicate,
-  list("`msg` must be a string" ~ msg) ~ purrr::is_scalar_character
-)
-make_check_ <- function(msg, p) {
-  force(msg)
-  p_symb <- if (purrr::is_formula(p)) lazyeval::f_rhs(p) else substitute(p)
+localize_check_ <- function(chk) {
+  msg <- lazyeval::f_eval_lhs(chk)
+  rhs <- lazyeval::f_rhs(chk)
+  env <- lazyeval::f_env(chk)
 
-  function(...) {
+  chkr <- function(...) {
     args <- eval(substitute(alist(...)))
-    nms <- vapply(args, deparse_collapse, character(1))
     parent <- parent.frame()
-    lhs <- unfurl_args(msg, nms, args, parent)
+    lhs <- lapply(args, function(arg) {
+      nm <- deparse_collapse(arg)
+      err <- paste(msg, encodeString(nm, quote = "`"), sep = ": ")
+      lazyeval::f_new(arg, err, parent)
+    })
 
-    lazyeval::f_new(p_symb, lhs, parent)
+    lazyeval::f_new(rhs, lhs, env)
   }
+
+  structure(chkr, class = c("strict_checker", class(chkr)))
 }
 
-#' Customize check functions
+#' Localize a global check formula
 #'
-#' @param p Predicate function or lambda expression.
-#' @param msg String.
-#' @return Formula
+#' @param chk Global check formula.
+#' @return Local check formula.
 #' @examples
 #' \dontrun{
-#' is_number <- make_check(purrr::is_scalar_numeric, "Not a number")
+#' is_number <- localize_check("Not a number" ~ purrr::is_scalar_numeric)
 #' secant <- function(f, x, dx) (f(x + dx) - f(x)) / dx
 #' secant <- strictly(secant, is_number(x, dx))
 #' }
 #' @export
-#' @name make_check
-make_check <- strictly_(make_check_, .checklist = chk_mc, .warn_missing = TRUE)
+#' @name localize_check
+localize_check <- strictly_(
+  localize_check_,
+  list("`chk` must be a formula of the form <string> ~ <predicate>" ~ chk) ~
+    is_gbl_check_formula,
+  .warn_missing = TRUE
+)
+
+#' @export
+print.strict_checker <- function(x, ...) {
+  env <- environment(x)
+  p <- env$rhs
+
+  cat("<strict_checker>\n")
+
+  cat("\n* Predicate function:\n")
+  p <- if (is_lambda(p)) expr_lambda(p) else p
+  cat(deparse_collapse(p), "\n")
+
+  cat("\n* Error message:\n")
+  cat(encodeString(env$msg, quote = "\""), "\n")
+}
 
 # make_assertthat_check()
