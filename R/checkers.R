628b63fe091a7d01bdde3af87494ef6dd8cf4048
Eugene Ha <eha@posteo.de>
2017-02-20 08:33:32 +0100

    Remove minor blemishes

--- R/checkers.R
+++ R/checkers.R
@@ -9,7 +9,8 @@ localize_nm <- function(nm, what_is, ns, env) {
 }
 
 scrub <- function(ptrn_rm, ptrn_sep) {
-  force(ptrn_rm); force(ptrn_sep)
+  force(ptrn_rm)
+  force(ptrn_sep)
 
   function(x) gsub(ptrn_sep, " ", gsub(ptrn_rm, "", x))
 }
@@ -24,18 +25,16 @@ make_vld_chkrs <- function(ns, pattern, sep, env = parent.frame()) {
 }
 
 chkrs_purrr <- make_vld_chkrs("purrr", pattern = "^is_", sep = "_")
-for (nm in names(chkrs_purrr))
-  assign(nm, chkrs_purrr[[nm]])
+for (nm in names(chkrs_purrr)) assign(nm, chkrs_purrr[[nm]])
 
 #' Make local check formulae
 #'
 #' Check makers derived from purrr predicate functions.
 #'
 #' @evalRd rd_alias(names(chkrs_purrr))
-#' @evalRd rd_usage(c(names(chkrs_purrr)))
+#' @evalRd rd_usage(names(chkrs_purrr))
 #' @param ... Expressions of function arguments to check.
 #' @return Check formula.
-#'
 #' @rawNamespace exportPattern("^vld_.*$")
 #' @name checkers
 NULL
