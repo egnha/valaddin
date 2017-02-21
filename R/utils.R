378c60e37ddc75eed63bf8d92113c047d4f3b1ac
Eugene Ha <eha@posteo.de>
2017-02-21 22:35:39 +0100

    Move deparse_collapse() to utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,20 @@
 # Null-default operator
 `%||%` <- purrr::`%||%`
 
-skip <- function(...) {}
-
 # Aliases consistent with our style convention
 is_true <- isTRUE
 is_false <- function(x) identical(FALSE, x)
 
-# An error object inherits from "condition"
+skip <- function(...) {}
+
 is_error <- function(x) {
   wh <- inherits(x, c("error", "condition"), which = TRUE)
   wh[1L] > 0L && wh[2L] > wh[1L]
+}
+
+# Deparse a language object as a single string
+deparse_collapse <- function(x) {
+  paste(trimws(deparse(x), which = "both"), collapse = "")
 }
 
 # Collapse a character vector into an enumerated string
