0743c367f074cab92492a2a5afbd3bba622cf52e
Eugene Ha <eha@posteo.de>
2017-02-23 07:59:30 +0100

    Updated tests for localized checkers

--- tests/testthat/test-checkers.R
+++ tests/testthat/test-checkers.R
@@ -1,18 +1,38 @@
-context("Checkers")
+context("Localized checkers")
 
-test_that("localized purrr checkers use corresponding purrr predicates", {
-  prefix <- "^vld_"
-  nms_valaddin <- grep(prefix, getNamespaceExports("valaddin"), value = TRUE)
-  nms_purrr <- sub(prefix, "is_", nms_valaddin)
-  nms <- purrr::transpose(list(valaddin = nms_valaddin, purrr = nms_purrr))
-  preds <- lapply(nms, function(.) {
-    lcl <- getExportedValue("valaddin", .$valaddin)
-    list(
-      valaddin = lazyeval::f_eval_rhs(globalize(lcl)),
-      purrr    = getExportedValue("purrr", .$purrr)
-    )
-  })
+prefix <- "^vld_"
+nms_valaddin <- grep(prefix, getNamespaceExports("valaddin"), value = TRUE)
+nms_purrr <- sub(prefix, "is_", nms_valaddin)
+chkrs <- purrr::transpose(
+  list(
+    chkr = lapply(nms_valaddin, getExportedValue, ns = "valaddin"),
+    nm   = nms_purrr
+  )
+)
 
-  for (p in preds)
-    expect_identical(p$valaddin, p$purrr)
+test_that("purrr checker class is 'check_maker'", {
+  is_check_maker <- map_lgl(chkrs, ~ inherits(.$chkr, "check_maker"))
+  expect_true(all(is_check_maker))
+})
+
+test_that("purrr checker predicate is corresponding purrr predicate", {
+  for (. in chkrs) {
+    pred_chkr  <- lazyeval::f_eval_rhs(globalize(.$chkr))
+    pred_purrr <- getExportedValue("purrr", .$nm)
+
+    expect_identical(pred_chkr, pred_purrr)
+  }
+})
+
+test_that("purrr checker error message is derived from purrr predicate name", {
+  f <- function(x) NULL
+  bad_arg <- log  # Every purrr predicate returns FALSE for bad_arg
+
+  for (. in chkrs) {
+    msg_chkr  <- lazyeval::f_eval_lhs(globalize(.$chkr))
+    msg_purrr <- sprintf("Not %s", gsub("_", " ", sub("^is_", "", .$nm)))
+
+    expect_identical(msg_chkr, msg_purrr)
+    expect_error(strictly(f, .$chkr(x))(bad_arg), msg_purrr)
+  }
 })
