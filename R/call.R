97cc9fe69a64c49da491db00818e8c50d281f376
Eugene Ha <eha@posteo.de>
2017-03-15 06:30:06 +0100

    More consistent function organization

--- R/call.R
+++ R/call.R
@@ -1,3 +1,12 @@
+express_lambda <- function(body) {
+  call("function", as.pairlist(alist(. = )), body)
+}
+
+deparse_call <- function(chk_item, fn_expr) {
+  call <- substitute(f(x), list(f = fn_expr, x = lazyeval::f_rhs(chk_item)))
+  deparse_collapse(call)
+}
+
 # Call a function against a given call signature
 call_fn <- function(.f) {
   force(.f)
@@ -8,7 +17,7 @@ call_fn <- function(.f) {
   }
 }
 
-# Rewrite the argument signature of a function
+# Rewire the argument signature of a function
 with_sig <- function(.f, .sig, .attrs) {
   f <- eval(call("function", .sig, body(.f)))
   environment(f) <- environment(.f)
--- R/firmly.R
+++ R/firmly.R
@@ -12,15 +12,6 @@ unfurl <- function(.symb, .nm, .msg, .env) {
   chk_items
 }
 
-express_lambda <- function(body) {
-  call("function", as.pairlist(alist(. = )), body)
-}
-
-deparse_call <- function(chk_item, fn_expr) {
-  call <- substitute(f(x), list(f = fn_expr, x = lazyeval::f_rhs(chk_item)))
-  deparse_collapse(call)
-}
-
 # Assemble a data frame of checks from a check formula
 assemble <- function(.chk, .nm, .symb, .env = lazyeval::f_env(.chk)) {
   p <- lazyeval::f_rhs(.chk)
