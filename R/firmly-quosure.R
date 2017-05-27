e031fb6cb516119fac87a70fb57d75d48c66d594
Eugene Ha <eha@posteo.de>
2017-05-27 10:15:01 +0300

    Parse quosure-based checks

--- R/firmly-quosure.R
+++ R/firmly-quosure.R
@@ -22,58 +22,80 @@ vld <- function(..., checklist = NULL) {
   }
 }
 
+parse_check <- function(chk, msg, syms) {
+  chk_ <- rlang::eval_tidy(chk)
+  is_local <- rlang::is_quosures(chk_)
+  if (is_local) {
+    pred <- lambda(chk_[[1L]])
+    qs <- chk_[-1L]
+  } else {
+    pred <- lambda(chk)
+    qs <- lapply(syms, rlang::new_quosure, env = baseenv())
+  }
+  text <- deparse_check(pred, qs, msg)
+  validation_df(pred, qs, text)
+}
+
 # Variation of rlang::as_function
 lambda <- function(q) {
-  bdy <- rlang::get_expr(q)
-  if (is_lambda(bdy)) {
-    expr <- call("function", as.pairlist(alist(.x = , .y = , . = .x)), bdy)
+  body <- rlang::get_expr(q)
+  if (is_lambda(body)) {
+    expr <- call("function", as.pairlist(alist(.x = , .y = , . = .x)), body)
     rlang::new_quosure(expr, rlang::get_env(q))
   } else {
     q
   }
 }
 
-q_deparse_call <- function(q, args) {
-  rlang::quo_text(rlang::expr(UQE(q)(UQ(args))))
+deparse_check <- function(pred, qs, msg) {
+  calls <- vapply(qs, q_deparse_call, character(1), q = pred)
+  msgs <- names(qs) %||% character(length(qs))
+  not_named <- !nzchar(msgs)
+  msgs[not_named] <- default_msg(msg, qs[not_named], calls[not_named])
+  list(call = calls, msg = msgs)
 }
 
-default_msg <- function(q, exprs, default) {
-  if (nzchar(default)) {
-    text <- vapply(exprs, rlang::quo_text, character(1))
-    paste(default, encodeString(text, quote = "`"), sep = ": ")
+q_deparse_call <- function(q, arg) {
+  rlang::quo_text(rlang::expr(UQE(q)(UQ(arg))))
+}
+
+default_msg <- function(msg, qs, calls) {
+  if (nzchar(msg)) {
+    vapply(qs, glue_esc_dot, character(1), text = msg)
   } else {
-    text <- vapply(exprs, q_deparse_call, character(1), q = q)
-    ws <- ifelse(grepl("\n", text), "\n", " ")
-    paste0("FALSE:", ws, text)
+    message_false(calls)
   }
 }
 
-validation_df <- function(q, exprs, msgs) {
+#' Glue-interpolate escaped dots
+#'
+#' @param q Quosure.
+#' @param text Text to interpolate.
+#' @return Glue object.
+#' @examples
+#' glue_esc_dot("The value of '{{.}}' is {.}", rlang::quo(x))
+#' #> The value of 'x' is {.}
+#' @noRd
+glue_esc_dot <- function(text, q) {
+  glue::glue(glue::glue(text, . = "{{.}}"), . = rlang::quo_text(q))
+}
+
+message_false <- function(calls) {
+  ws <- ifelse(grepl("\n", calls), "\n", " ")
+  paste0("FALSE:", ws, calls)
+}
+
+validation_df <- function(pred, exprs, text) {
   n <- length(exprs)
   d <- list(
-    pred = `[<-`(vector("list", n), list(q)),
+    pred = `[<-`(vector("list", n), list(pred)),
     expr = exprs,
-    msg  = msgs
+    call = text$call,
+    msg  = text$msg
   )
   class(d) <- "data.frame"
   attr(d, "row.names") <- .set_row_names(n)
   d
-}
-
-parse_check <- function(quo_chk, syms) {
-  q <- lambda(quo_chk[[1]])
-
-  if (length(quo_chk) == 1) {
-    exprs <- lapply(syms, rlang::new_quosure, env = emptyenv())
-  } else {
-    exprs <- quo_chk[-1]
-  }
-
-  msgs <- names(exprs) %||% character(length(exprs))
-  not_named <- !nzchar(msgs)
-  msgs[not_named] <- default_msg(q, exprs[not_named], names(quo_chk)[1])
-
-  validation_df(q, exprs, msgs)
 }
 
 safely_rename <- function(..., avoid) {
