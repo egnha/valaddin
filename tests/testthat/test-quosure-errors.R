0c4cc685326ab76478e5992561759f8a7e359e35
Eugene Ha <eha@posteo.de>
2017-07-02 09:48:30 +0200

    Adapt error message tests to input-validation checks as definitions

--- tests/testthat/test-quosure-errors.R
+++ tests/testthat/test-quosure-errors.R
@@ -7,38 +7,38 @@ test_that("unnamed global check uses auto-generated error messages", {
 })
 
 test_that("unnamed local check uses auto-generated error messages", {
-  f <- firmly(function(x, y) NULL, isTRUE ~ quos(x, y))
+  f <- firmly(function(x, y) NULL, isTRUE ~ vld(x, y))
   expect_error(f(FALSE, TRUE), errmsg_false("isTRUE(x)"))
   expect_error(f(TRUE, FALSE), errmsg_false("isTRUE(y)"))
 })
 
 test_that("named global check uses name as error message", {
-  f <- firmly(function(x, y) NULL, "Not true" = isTRUE)
+  f <- firmly(function(x, y) NULL, "Not true" := isTRUE)
   expect_n_errors(1, f, list(x = FALSE, y = TRUE), "Not true")
   expect_n_errors(2, f, list(x = FALSE, y = FALSE), "Not true")
 })
 
 test_that("named local check uses name as error message", {
-  f <- firmly(function(x, y) NULL, "Not true" = isTRUE ~ quos(x, y))
+  f <- firmly(function(x, y) NULL, "Not true" := isTRUE ~ vld(x, y))
   expect_n_errors(1, f, list(x = FALSE, y = TRUE), "Not true")
   expect_n_errors(1, f, list(x = TRUE, y = FALSE), "Not true")
   expect_n_errors(2, f, list(x = FALSE, y = FALSE), "Not true")
 })
 
 test_that("name of local expression overrides name of check", {
-  f <- firmly(function(x, y) NULL, "global" = isTRUE ~ quos("local" = x, y))
+  f <- firmly(function(x, y) NULL, "global" := isTRUE ~ vld("local" := x, y))
   expect_error(f(FALSE, TRUE), "local")
   expect_error(f(TRUE, FALSE), "global")
 })
 
 test_that("name of local expression overrides auto-generated error message", {
-  f <- firmly(function(x, y) NULL, isTRUE ~ quos("local" = x, y))
+  f <- firmly(function(x, y) NULL, isTRUE ~ vld("local" := x, y))
   expect_error(f(FALSE, TRUE), "local")
   expect_error(f(TRUE, FALSE), errmsg_false("isTRUE(y)"))
 })
 
 test_that("auto-message is used if error message fails to be created", {
-  f <- firmly(function(x, y) NULL, "{stop('!')}" = isTRUE ~ x)
+  f <- firmly(function(x, y) NULL, "{stop('!')}" := isTRUE ~ x)
   expect_error(f(FALSE), errmsg_false("isTRUE(x)"))
   expect_error(f(FALSE), "Error interpolating message")
 })
@@ -53,7 +53,7 @@ test_that("error messages of named global check interpolate dot", {
     is_scalar <- function(x) length(x) == 1L
     firmly(
       function(x, y) NULL,
-      "{{s_quote(.)}} is not a scalar (length is {length(.)})" = is_scalar
+      "{{s_quote(.)}} is not a scalar (length is {length(.)})" := is_scalar
     )
   })
   expect_error(f(x = 1:3), esc_perl("'x' is not a scalar (length is 3)"))
@@ -61,15 +61,15 @@ test_that("error messages of named global check interpolate dot", {
 })
 
 test_that("dot in global check message always stands for current argument", {
-  f <- firmly(function(x, .) NULL, "literal: {{.}}; value: {.}" = isTRUE)
+  f <- firmly(function(x, .) NULL, "literal: {{.}}; value: {.}" := isTRUE)
   expect_error(f(TRUE, "dot-value"), "literal: .; value: dot-value")
   expect_error(f("x-value", TRUE), "literal: x; value: x-value")
 })
 
 test_that("dot in local check message does not stand for current argument", {
   f <- firmly(function(x, .) NULL,
-              "literal: {{.}}, value: {.}" = isTRUE ~
-                quos("x: {x}, dot: {.}" = x, .))
+              "literal: {{.}}, value: {.}" := isTRUE ~
+                vld("x: {x}, dot: {.}" := x, .))
   expect_error(f(TRUE, "dot"), "literal: ., value: dot")
   expect_error(f("x", TRUE), "x: x, dot: TRUE")
 })
@@ -80,8 +80,8 @@ test_that("error messages of named local check interpolate dot", {
     is_scalar <- function(x) length(x) == 1L
     firmly(
       function(x, y) NULL,
-      "{{s_quote(.)}} is not a scalar (length is {length(.)})" =
-        is_scalar ~ quos(x, x - y)
+      "{{s_quote(.)}} is not a scalar (length is {length(.)})" :=
+        is_scalar ~ vld(x, x - y)
     )
   })
   expect_error(f(1:2),
@@ -104,21 +104,23 @@ test_that("error messages of unnamed local check don't interpolate dot", {
   expect_error(f(TRUE, FALSE), NA)
 })
 
-test_that("name of global check is interpolated in predicate scope", {
-  pred <- local({
+test_that("message of global check is interpolated in the messages's scope", {
+  chk <- local({
     bar <- function(x) sprintf("local text: %s", x)
-    rlang::quo(isTRUE)
+    vld("{{bar(.)}}; value: {.}" := isTRUE)
   })
-  f <- firmly(function(x) NULL, "{{bar(.)}}; value: {.}" = !! pred)
+  bar <- function(x) "bogus"  # should be ignored by string interpolation
+  f <- firmly(function(x) NULL, UQS(chk))
+  expect_error(f(TRUE), NA)
   expect_error(f("not true"), "local text: x; value: not true")
 })
 
-test_that("name of check item is interpolated in check-item scope", {
+test_that("message of check item is interpolated in the messages's scope", {
   chk <- local({
-    a <- "local"
-    rlang::quo(x)
+    message <- "local message"
+    isTRUE ~ vld("{message}" := x)
   })
-  f <- firmly(function(x, y) NULL,
-              isTRUE ~ quos("x is {x}, a is {a}, y is {y}" = !! chk))
-  expect_error(f(FALSE, "y"), "x is FALSE, a is local, y is y")
+  message <- "bogus"  # should be ignored by string interpolation
+  f <- firmly(function(x, y) NULL, chk)
+  expect_error(f(FALSE, "y"), "local message")
 })
