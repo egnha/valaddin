1e17f8ac2c99f0e14fefca179255d574108bafb7
Eugene Ha <eha@posteo.de>
2017-07-05 10:35:18 +0200

    Update doc for component extractors

--- R/components.R
+++ R/components.R
@@ -1,31 +1,30 @@
 #' Decompose a firmly applied function
 #'
-#' Decompose a firmly applied function (i.e., a function created by
-#' \code{\link{firmly}}):
-#' \itemize{
-#'   \item \code{firm_core} extracts the underlying \dQuote{core}
-#'     function—the function that is called when all arguments are valid.
-#'   \item \code{firm_checks} extracts the checks.
-#'   \item \code{firm_error} extracts the subclass of the error condition that
-#'     is signaled when an input validation error occurs.
-#' }
+#' Decompose a firmly applied function (i.e., a function created by [firmly()]
+#' or [fasten()]):
+#' - `firm_core()` extracts the underlying \dQuote{core} function—the function
+#'   that is called when all arguments are valid.
+#' - `firm_checks()` extracts the checks.
+#' - `firm_error()` extracts the subclass of the error condition that is
+#'   signaled when an input validation error occurs.
 #'
+#' @details For primitive functions, `firm_core()` returns the underlying
+#'   function as a closure.
 #' @param x Object to decompose.
-#' @return If \code{x} is a firmly applied function:
-#'   \itemize{
-#'     \item \code{firm_core} returns a function.
-#'     \item \code{firm_checks} returns a data frame with components \code{pred}
-#'       (quosure), \code{expr} (quosure), \code{call} (character), \code{msg}
-#'       (character), \code{is_msg_gbl} (logical), \code{env_msg} (environment).
-#'     \item \code{firm_error} returns a character vector.
-#'   }
-#'   In the absence of the component to be extracted, these functions return
-#'   \code{NULL}.
+#' @return If `x` is a firmly applied function:
+#'   - `firm_core()` returns a function.
+#'   - `firm_checks()` returns a data frame with components `pred`
+#'     (quosure), `expr` (quosure), `call` (character), `msg`
+#'     (character), `is_msg_gbl` (logical), `env_msg` (environment).
+#'   - `firm_error()` returns a character vector.
 #'
-#' @seealso \code{\link{firmly}}
+#'   In the absence of the component to be extracted, these functions return
+#'   `NULL` (in particular, whenever `x` is not of class `firm_closure`).
+#'
+#' @seealso [firmly()]
 #' @examples
 #' f <- function(x, y, ...) NULL
-#' ff <- firmly(f, is.numeric, {. > 0} ~ quos(x, y - x))
+#' ff <- firmly(f, is.numeric, {isTRUE(. > 0)} ~ vld(x, y - x))
 #' identical(firm_core(ff), f)  # [1] TRUE
 #' firm_checks(ff)              # 4 x 6 data frame
 #' firm_error(ff)               # [1] "inputValidationError"
