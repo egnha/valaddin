9e16499d11790ab577619a67ec2443e99c4daf49
Eugene Ha <eha@posteo.de>
2017-07-17 08:10:58 +0200

    Consolidate localize() and localize_comparison(), simplify signature
    The call signature for localize() is now consistent with the syntax for
    checks supplied to firmly().

--- R/scope.R
+++ R/scope.R
@@ -2,29 +2,23 @@
 #'
 #' Given a predicate (function), `localize()` returns a function that
 #' _generates_ input validation checks (of local scope) for the predicate.
-#' `globalize()` takes such a check generator and returns the underlying
-#' predicate (wrapped in [vld()]); when `globalize()` is unquoted in a call to
+#' `globalize()` takes such a check generator and returns the underlying error
+#' message-predicate pair. When `globalize()` is unquoted in a call to
 #' [firmly()] or [fasten()], the corresponding predicate is applied as an input
 #' validation check of global scope.
 #' \cr\cr
-#' The function `predicate_function()`, resp. `predicate_message()`, extracts
-#' the associated predicate function, resp. error message.
+#' The functions `predicate_function()` and `predicate_message()` extract the
+#' associated predicate function and error message.
 #'
-#' @param p Predicate function of one argument, for `localize()`, of two or more
-#'   arguments for `localize_comparison()`.
-#' @param msg,value Error message (string). An empty string (the default)
-#'   implies that error messages are to be automatically generated.
-#' @param x Object of class `local_predicate` or `global_predicate`.
-#'
-#' @details Conceptually, `localize()` and `globalize()` represent inverse
-#'   operations. However, they are _not_ mutually invertible as functions,
-#'   because they are not composable.
+#' @details `globalize()` approximately inverts `localize()`, by returning the
+#'   underlying message-predicate pair as a named list, rather than as a
+#'   bare predicate function or definition (formula).
 #'
 #' @examples
 #' \dontrun{
 #'
 #' ## Make a positivity checker
-#' chk_positive <- localize({isTRUE(. > 0)}, "{{.}} is not positive")
+#' chk_positive <- localize("{{.}} is not positive" := {isTRUE(. > 0)})
 #' f <- firmly(function(x, y) "Pass", chk_positive(x, x - y))
 #'
 #' f(2, 1)
@@ -34,13 +28,13 @@
 #' #> Error: f(x = 1, y = 2)
 #' #> x - y is not positive
 #'
-#' ## Make a length checker, parameterized by .ref
+#' ## Make a parameterized length checker
 #' chk_length <-
-#'   localize_comparison(
-#'     {length(.) == .ref}, "{{.}} not of length {{{.ref$value}}}"
+#'   localize(
+#'     "{{.}} not of length {{l}}" := function(., l) length(.) == l
 #'   )
-#' ## .ref gets the value 2 (so check that the length is 2)
-#' g <- firmly(function(x, y) "Pass", chk_length(2)(y))
+#' ## l gets the value 2 (so check that the length is 2)
+#' g <- firmly(function(x, y) "Pass", chk_length(l = 2, y))
 #'
 #' g(1, 1:2)
 #' #> [1] "Pass"
@@ -55,96 +49,78 @@ NULL
 
 #' @rdname scope
 #' @export
-localize <- fasten(
-  "'msg' must be a string" := {is.character(.) && length(.) == 1} ~ msg
-)(
-  function(p, msg = "") {
-    q <- rlang::enquo(p)
-    x <- rlang::quo_expr(q)
-    pred <- as_predicate(q, capture_env(q, parent.frame()))
-    expr <- if (is_lambda(x)) pred$expr else x
-    localize_(pred$fn, expr, msg)
-  }
-)
-localize_ <- function(fn, expr, msg) {
-  force(fn)
-  force(expr)
-  force(msg)
+#' @param p Predicate function, or a definition, whose LHS is an error message
+#'   (string) and whose RHS is a predicate function.
+localize <- function(p) {
+  q <- rlang::enquo(p)
+  check <- as_check(q)
+  pred <- as_predicate(check$chk, rlang::f_env(check$chk))
   structure(
     function(...) {
-      fml <- rlang::new_formula(fn, vld(...), parent.frame())
+      args <- categorize_args(...)
+      fn <- partial(pred$fn, UQS(args$defvals))
+      # TODO: interpolate the message
+      check$chk <- rlang::new_formula(fn, vld(UQS(args$exprs)), parent.frame())
       structure(
-        (if (nzchar(msg)) vld(msg := fml) else vld(fml))[[1]],
-        vld_pred_expr = expr,
+        check,
+        vld_pred_expr = as.call(c(pred$expr, args$defvals)),
         class = "local_validation_checks"
       )
     },
-    class = c("local_predicate", "function")
+    class = "local_predicate"
   )
+}
+categorize_args <- function(...) {
+  dd <- rlang::dots_definitions(...)
+  is_named <- nzchar(names(dd$dots))
+  list(
+    defvals = lapply(dd$dots[is_named], rlang::eval_tidy),
+    exprs   = vld_(dd$dots[!is_named], dd$defs)
+  )
+}
+as_check <- function(q) {
+  if (rlang::is_definition(rlang::f_rhs(q))) {
+    def <- rlang::f_rhs(q)
+    env <- rlang::f_env(q)
+    list(
+      msg = rlang::new_quosure(rlang::f_lhs(def), env),
+      chk = rlang::new_quosure(rlang::f_rhs(def), env)
+    )
+  } else
+    set_empty_msg(q)
 }
 
 is_local_predicate <- identify_class("local_predicate")
 
 #' @rdname scope
 #' @export
-localize_comparison <- fasten(
-  "'msg' must be a string" := {is.character(.) && length(.) == 1} ~ msg
-)(
-  function(p, msg = "") {
-    force(msg)
-    q <- rlang::enquo(p)
-    env <- capture_env(q, parent.frame())
-    cmp <- as_comparison(q, env)
-    function(.ref, ...) {
-      repr <- list(value = .ref, expr = deparse_collapse(substitute(.ref)))
-      msg <- glue_text(msg, env, list(.ref = repr),
-                       .open = "{{{", .close = "}}}")
-      localize_(cmp$fn(.ref, ...), cmp$expr, msg)
-    }
-  }
-)
-as_comparison <- function(p, env) {
-  expr <- rlang::f_rhs(p)
-  if (is_lambda(expr)) {
-    expr <- new_fn_expr(expr, alist(. = , .ref = ref))
-  } else {
-    f <- try_eval_tidy(p, env)
-    if (!is.function(f)) {
-      stop(err_not_function(expr, maybe_error(f)), call. = FALSE)
-    }
-    expr <- new_fn_expr(bquote(.(f)(., ref, ...)))
-    env <- environment(f)
-  }
-  fn <- eval(new_fn_expr(expr, alist(ref = , ... = )), env)
-  list(expr = expr, fn = fn)
-}
-
-#' @param chkr Function of class `local_predicate`, i.e., a function created by
+#' @param lp Function of class `local_predicate`, i.e., a function created by
 #'   `localize()`.
-#' @rdname scope
-#' @export
 globalize <- fasten(
-  "'chkr' must be a local predicate (see ?localize)" :=
-    is_local_predicate ~ chkr
+  "'lp' must be a local predicate (see ?localize)" :=
+    is_local_predicate ~ lp
 )(
-  function(chkr) {
-    env <- environment(chkr)
+  function(lp) {
+    pred <- environment(lp)$pred
+    check <- environment(lp)$check
+    check$chk <- rlang::new_quosure(pred$fn, rlang::f_env(check$chk))
     structure(
-      (if (nzchar(env$msg)) vld(env$msg := env$fn) else vld(env$fn))[[1]],
-      vld_pred_expr = env$expr,
-      class = c("global_predicate", "function")
+      check,
+      vld_pred_expr = pred$expr,
+      class = "global_predicate"
     )
   }
 )
 
 #' @rdname scope
 #' @export
+#' @param x Object of class `local_predicate` or `global_predicate`.
 predicate_function <- function(x) {
   UseMethod("predicate_function")
 }
 #' @export
 predicate_function.local_predicate <- function(x) {
-  environment(x)$fn
+  environment(x)$pred$fn
 }
 #' @export
 predicate_function.global_predicate <- function(x) {
@@ -158,7 +134,7 @@ predicate_message <- function(x) {
 }
 #' @export
 predicate_message.local_predicate <- function(x) {
-  environment(x)$msg
+  rlang::eval_tidy(environment(x)$check$msg)
 }
 #' @export
 predicate_message.global_predicate <- function(x) {
@@ -167,12 +143,13 @@ predicate_message.global_predicate <- function(x) {
 
 #' @rdname scope
 #' @export
+#' @param value Error message (string).
 `predicate_message<-` <- function(x, value) {
   UseMethod("predicate_message<-")
 }
 #' @export
 `predicate_message<-.local_predicate` <- function(x, value) {
-  environment(x)$msg <- value
+  rlang::f_rhs(environment(x)$check$msg) <- value
   invisible(x)
 }
 #' @export
@@ -185,7 +162,7 @@ predicate_message.global_predicate <- function(x) {
 print.local_predicate <- function(x, ...) {
   cat("<local_predicate>\n")
   cat("\n* Predicate function:\n")
-  cat(deparse_collapse(environment(x)$expr), "\n")
+  cat(deparse_collapse(environment(x)$pred$expr), "\n")
   cat("\n* Error message:\n")
   cat(encodeString(predicate_message(x), quote = "\""), "\n")
   invisible(x)
