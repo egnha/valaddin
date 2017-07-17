e95cffc7c5494796d96a61898d6d0f25409b9576
Eugene Ha <eha@posteo.de>
2017-07-17 08:47:17 +0200

    Improve code readability by ordering functions top-down

--- R/validation.R
+++ R/validation.R
@@ -1,39 +1,3 @@
-name_predicates <- function(suffixes, exprs) {
-  paste0(safely_rename("pred", avoid = exprs), suffixes)
-}
-
-safely_rename <- function(nm, avoid) {
-  n <- max(unlist(lapply(avoid, function(expr) rapply(as.list(expr), nchar))))
-  filler <- paste(character(n), collapse = "_")
-  paste(nm, filler, sep = "_")
-}
-
-bind_predicates <- function(nms, preds) {
-  env <- new.env(parent = emptyenv())
-  for (i in seq_along(nms)) {
-    assign(nms[i], preds[[i]], envir = env)
-  }
-  env
-}
-
-bind_promises <- function(nms, exprs, env_eval, parent) {
-  env_assign <- new.env(parent = parent)
-  for (i in seq_along(nms))
-    eval(bquote(
-      delayedAssign(.(nms[[i]]), .(exprs[[i]]), env_eval, env_assign)
-    ))
-  env_assign
-}
-
-express_check <- function(exprs, nms) {
-  lapply(seq_along(exprs), function(i)
-    list(
-      expr = rlang::expr(UQ(as.name(nms[[i]]))(UQE(exprs[[i]]))),
-      env  = rlang::get_env(exprs[[i]])
-    )
-  )
-}
-
 #' @importFrom rlang eval_bare
 validation_closure <- function(f, chks, sig, arg, error_class) {
   force(f)
@@ -88,6 +52,38 @@ validation_closure <- function(f, chks, sig, arg, error_class) {
   }
 }
 
+name_predicates <- function(suffixes, exprs) {
+  paste0(safely_rename("pred", avoid = exprs), suffixes)
+}
+safely_rename <- function(nm, avoid) {
+  n <- max(unlist(lapply(avoid, function(expr) rapply(as.list(expr), nchar))))
+  filler <- paste(character(n), collapse = "_")
+  paste(nm, filler, sep = "_")
+}
+bind_predicates <- function(nms, preds) {
+  env <- new.env(parent = emptyenv())
+  for (i in seq_along(nms)) {
+    assign(nms[i], preds[[i]], envir = env)
+  }
+  env
+}
+bind_promises <- function(nms, exprs, env_eval, parent) {
+  env_assign <- new.env(parent = parent)
+  for (i in seq_along(nms))
+    eval(bquote(
+      delayedAssign(.(nms[[i]]), .(exprs[[i]]), env_eval, env_assign)
+    ))
+  env_assign
+}
+express_check <- function(exprs, nms) {
+  lapply(seq_along(exprs), function(i)
+    list(
+      expr = rlang::expr(UQ(as.name(nms[[i]]))(UQE(exprs[[i]]))),
+      env  = rlang::get_env(exprs[[i]])
+    )
+  )
+}
+
 problems <- function(chks, verdict, env) {
   vapply(seq_along(verdict), function(i) {
     out <- verdict[[i]]
@@ -99,7 +95,6 @@ problems <- function(chks, verdict, env) {
       err_invalid_value(chks$call[[i]], out)
   }, character(1))
 }
-
 err_invalid_input <- function(., env) {
   parent.env(env) <- .$env_msg[[1]]
   env_msg <- if (.$is_msg_gbl[[1]]) bind_as_dot(.$expr[[1]], env) else env
@@ -116,22 +111,18 @@ err_invalid_input <- function(., env) {
     err_msg_error(.$call[[1]], .$msg[[1]], not_string)
   }
 }
-
 bind_as_dot <- function(q, env) {
   env_dot <- new.env(parent = env)
   eval(bquote(delayedAssign(".", .(rlang::quo_expr(q)), env, env_dot)))
   env_dot
 }
-
 err_msg_error <- function(call, msg, err) {
   sprintf('%s\n[Error interpolating message "%s": %s]',
           message_false(call), msg, err)
 }
-
 err_eval_error <- function(call, out) {
   sprintf("Error evaluating check %s: %s", call, conditionMessage(out))
 }
-
 err_invalid_value <- function(call, out) {
   sprintf("Predicate value %s not TRUE/FALSE: %s", call, deparse_collapse(out))
 }
