7c99b883f92bfa2b1ae984db515576c25a51693b
Eugene Ha <eha@posteo.de>
2017-08-19 11:16:52 +0200

    Adapt parser to checker() and new vld()

--- R/parser.R
+++ R/parser.R
@@ -1,39 +1,55 @@
 #' Parse input validation checks
 #'
-#' @param chks List of input-validation checks as pairs of quosures (named
-#'   `msg`, `chk`), as output by [vld()].
-#' @return List of parsed input validation checks, separated by scope (global vs
-#'   local). Complete parsing of global checks is deferred until the formal
+#' @param chks List of input-validation checks, as output by [vld()].
+#'
+#' @return List of parsed input validation checks, separated by scope: global vs
+#'   local. Complete parsing of global checks is deferred until the formal
 #'   arguments of the function are known.
+#'
 #' @noRd
 parse_checks <- function(chks) {
-  if (length(chks) == 0)
+  if (rlang::is_empty(chks))
     return(NULL)
   chklist <- lapply(chks, parse_check)
-  is_global <- vapply(chklist, function(.) is.null(.$chk_items), logical(1))
+  is_global <- vapply(chklist, function(.) rlang::is_empty(.$chk_items), logical(1))
   list(
     global = chklist[is_global],
     local  = tabulate_checks(chklist[!is_global])
   )
 }
-parse_check <- function(chk) {
-  chkev <- try_eval_tidy(chk$chk)
-  if (rlang::is_formula(chkev)) {
-    env_fml <- rlang::f_env(chkev)
-    pred <- as_predicate(rlang::f_lhs(chkev), env_fml)
-    chk_items <- as_check_items(rlang::f_rhs(chkev), env_fml)
-  } else {
-    pred <- as_predicate(chk$chk, rlang::f_env(chk$chk))
-    chk_items <- NULL
-  }
-  msg <- rlang::eval_tidy(chk$msg)
+parse_check <- function(x) {
+  c(decompose_check(x), decompose_message(x$msg))
+}
+decompose_message <- function(msg) {
   list(
-    fn        = pred$fn,
-    expr      = (chk %@% "vld_pred_expr") %||% pred$expr,
-    chk_items = chk_items,
-    msg       = msg,
-    env_msg   = rlang::f_env(chk$msg)
+    msg = rlang::eval_tidy(msg),
+    env_msg = rlang::f_env(msg)
   )
+}
+decompose_check <- function(x) {
+  if (is_chkr_validation(x))
+    return(x[c("fn", "expr", "chk_items")])
+  chk <- x$chk
+  chk_eval <- try_eval_tidy(chk)
+  if (rlang::is_formula(chk_eval)) {
+    env <- rlang::f_env(chk_eval)
+    fml <- get_check_formula(chk, chk_eval)
+    c(
+      as_predicate(rlang::f_lhs(fml), env),
+      chk_items = list(as_check_items(rlang::f_rhs(fml), env))
+    )
+  } else
+    c(
+      as_predicate(chk, rlang::f_env(chk)),
+      chk_items = list(NULL)
+    )
+}
+get_check_formula <- function(chk, chk_eval) {
+  x <- rlang::f_rhs(chk)
+  if (rlang::is_formula(x))
+    x
+  else
+    chk_eval
 }
 as_check_items <- function(x, env) {
   if (is_vld_expr(x))
@@ -43,7 +59,7 @@ as_check_items <- function(x, env) {
   else
     list(set_empty_msg(rlang::new_quosure(x, env)))
 }
-is_vld_expr <- identify_caller("vld")
+is_vld_expr <- check_is_caller("vld")
 as_predicate <- function(q, env) {
   expr <- rlang::get_expr(q)
   if (is_lambda(expr))
@@ -84,65 +100,53 @@ maybe_error <- function(x) {
   else
     NULL
 }
-is_error <- identify_class("error")
+is_error <- check_is_class("error")
 
-#' Tabulate an input validation check as a data frame
-#'
-#' @details Adapted from
-#'   [`quickdf()`](http://adv-r.had.co.nz/Profiling.html#be-lazy).
-#' @param pred Predicate function.
-#' @param chk_quos List of expressions to validate, as quosures.
-#' @param text List of textual data for input validation:
-#'   - `call`: expression of a validation call (string)
-#'   - `msg`: error message (string)
-#'   - `is_msg_gbl`: is the error message derived from a global error message?
-#'   - `env`: environment in which to interpolate the error message
-#' @return Data frame of input-validation data to be used by
-#'   `validation_closure()`.
-#' @noRd
-validation_tbl <- function(pred, chk_quos, text) {
-  n <- length(chk_quos)
-  x <- list(
-    pred       = `[<-`(vector("list", n), list(pred)),
-    expr       = chk_quos,
-    call       = text$call,
-    msg        = text$msg,
-    env_msg    = text$env_msg,
-    is_msg_gbl = text$is_msg_gbl
+check_at_args <- function(args) {
+  quo_args <- lapply(args, function(.)
+    set_empty_msg(rlang::new_quosure(., emptyenv()))
   )
-  class(x) <- c("tbl_df", "tbl", "data.frame")
-  attr(x, "row.names") <- .set_row_names(n)
-  x
+  function(xs) {
+    for (i in seq_along(xs))
+      xs[[i]]$chk_items <- quo_args
+    tabulate_checks(xs)
+  }
 }
+
 tabulate_checks <- function(xs) {
-  parts <- lapply(xs, function(.) {
-    text <- deparse_check(.$expr, .$chk_items, .$msg, .$env_msg)
-    validation_tbl(.$fn, lapply(.$chk_items, `[[`, "chk"), text)
-  })
-  do.call("rbind", parts)
+  checks <- lapply(xs, tabulate_check)
+  do.call("rbind", checks)
 }
-deparse_check <- function(expr, chk_items, def_msg, env_msg) {
-  calls <-
-    vapply(chk_items, function(.) deparse_call(expr, .$chk), character(1))
-  msgs <-
-    vapply(chk_items, function(.) rlang::eval_tidy(.$msg), character(1))
+tabulate_check <- function(x) {
+  text <- deparse_check(x$expr, x$chk_items, x$msg, x$env_msg)
+  items <- lapply(x$chk_items, `[[`, "chk")
+  as_check_tbl(x$fn, items, text)
+}
+
+deparse_check <- function(expr, chk_items, msg_default, env_msg) {
+  calls <- vapply(chk_items, function(.) deparse_call(expr, .$chk), character(1))
+  msgs <- vapply(chk_items, function(.) rlang::eval_tidy(.$msg), character(1))
   is_gbl <- !nzchar(msgs)
   msgs[is_gbl] <-
-    make_message(def_msg, env_msg, chk_items[is_gbl], calls[is_gbl])
+    make_message(msg_default, env_msg, chk_items[is_gbl], calls[is_gbl])
   envs <- vector("list", length(chk_items))
   envs[ is_gbl] <- list(env_msg)
   envs[!is_gbl] <- lapply(chk_items[!is_gbl], function(.) rlang::f_env(.$msg))
-  list(call = calls, msg = msgs, is_msg_gbl = is_gbl, env_msg = envs)
+  list(
+    call       = calls,
+    msg        = msgs,
+    is_msg_gbl = is_gbl,
+    env_msg    = envs
+  )
 }
 deparse_call <- function(expr, arg) {
   expr_arg <- rlang::quo_expr(arg)
-  if (is_partial_fn_expr(expr))
+  if (is_chkr_predicate_expr(expr))
     call <- as.call(c(rlang::node_car(expr), expr_arg, rlang::node_cdr(expr)))
   else
     call <- as.call(c(expr, expr_arg))
   deparse_collapse(call)
 }
-is_partial_fn_expr <- is.call
 make_message <- function(msg, env_msg, chk_items, calls) {
   if (nzchar(msg))
     vapply(chk_items, function(.) glue_opp(.$chk, msg, env_msg), character(1))
@@ -161,15 +165,25 @@ message_false <- function(call) {
 #   "length of ‘x’: {length(.)}"
 glue_opp <- function(qdot, text, env) {
   glue_text(
-    text, env, list(. = rlang::quo_text(qdot)),
-    .open = "{{", .close = "}}"
+    text,
+    env,
+    list(. = rlang::quo_text(qdot)),
+    .open = "{{",
+    .close = "}}"
   )
 }
 
-localize_at <- function(xs, syms) {
-  qs <- lapply(syms, function(.)
-    set_empty_msg(rlang::new_quosure(., emptyenv())))
-  for (i in seq_along(xs))
-    xs[[i]]$chk_items <- qs
-  tabulate_checks(xs)
+as_check_tbl <- function(pred, items, text) {
+  n <- length(items)
+  x <- list(
+    pred       = `[<-`(vector("list", n), list(pred)),
+    expr       = items,
+    call       = text$call,
+    msg        = text$msg,
+    env_msg    = text$env_msg,
+    is_msg_gbl = text$is_msg_gbl
+  )
+  class(x) <- c("tbl_df", "tbl", "data.frame")
+  attr(x, "row.names") <- .set_row_names(n)
+  x
 }
