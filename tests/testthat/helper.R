66865aa96ff9b4b8cf98c508e7c95ffb406d4d97
Eugene Ha <eha@posteo.de>
2017-08-19 11:23:12 +0200

    Minor reorganization of test helpers

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,5 @@
 library(stringr, warn.conflicts = FALSE)
-
-`%>%` <- purrr::`%>%`
+library(purrr, warn.conflicts = FALSE)
 
 errmsg_false <- function(text) {
   esc_perl(message_false(text))
@@ -12,17 +11,6 @@ errmsg_invalid <- function(call, out) {
   esc_perl(err_invalid_value(call, out))
 }
 
-# Expect exactly n errors matching pattern
-expect_n_errors <- function(n, f, args, pattern) {
-  suppressWarnings(do.call(purrr::safely(f), args, quote = TRUE)) %>% {
-    stringr::str_count(.$error, pattern)
-  } %>%
-    expect_equal(n)
-}
-
-# Escape string for Perl-style regex
-esc_perl <- function(x) stringr::str_replace_all(x, "(\\W)", "\\\\\\1")
-
 # Adapt an expectation function to use Perl-style regex
 perlize <- function(f) {
   force(f)
@@ -30,4 +18,19 @@ perlize <- function(f) {
   function(object, regexp, ...) {
     f(object, esc_perl(regexp), perl = TRUE, ...)
   }
+}
+
+expect_error_perl <- function(...) {
+  expect_error(..., perl = TRUE)
+}
+
+# Escape string for Perl-style regex
+esc_perl <- function(x) stringr::str_replace_all(x, "(\\W)", "\\\\\\1")
+
+# Expect exactly n errors matching pattern
+expect_n_errors <- function(n, f, args, pattern) {
+  suppressWarnings(do.call(purrr::safely(f), args, quote = TRUE)) %>% {
+    stringr::str_count(.$error, pattern)
+  } %>%
+    expect_equal(n)
 }
