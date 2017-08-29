32bef7f759b316d2c2597cd83d1e4196cb6e5c2e
Eugene Ha <eha@posteo.de>
2017-08-29 08:24:10 +0200

    Update tests of fasten()

--- tests/testthat/test-fasten.R
+++ tests/testthat/test-fasten.R
@@ -15,8 +15,8 @@ test_that("error raised when error_class is not NULL or a character vector", {
 
 test_that("fasten() is a curried version of firmly()", {
   f <- function(x, y, ...) NULL
-  f1 <- firmly(f, "{{.}} isn't true" := isTRUE ~ vld(x, "y is not true" := y))
-  f2 <- fasten("{{.}} isn't true" := isTRUE ~ vld(x, "y is not true" := y))(f)
+  f1 <- firmly(f, "{{.}} isn't true" := isTRUE(x, "y is not true" := y))
+  f2 <- fasten("{{.}} isn't true" := isTRUE(x, "y is not true" := y))(f)
 
   expect_identical(NULL, expect_identical(f1(TRUE, TRUE), f2(TRUE, TRUE)))
   expect_error(f1(TRUE, 0), "y is not true")
