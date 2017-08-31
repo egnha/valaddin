75dae264259bcdcd9388c6be2e2940cf3bbb2ab4
Eugene Ha <eha@posteo.de>
2017-08-31 08:38:26 +0200

    Remove noise from doc index

--- R/components.R
+++ R/components.R
@@ -1,3 +1,9 @@
+firm_closure_extractor <- function(this) {
+  force(this)
+  function(x)
+    if (is_firm(x)) .subset2(environment(x), this) else NULL
+}
+
 #' Decompose a firmly applied function
 #'
 #' Decompose a firmly applied function (i.e., a function created by [firmly()]
@@ -9,7 +15,9 @@
 #'
 #' @details For primitive functions, `firm_core()` returns the underlying
 #'   function as a closure.
+#'
 #' @param x Object to decompose.
+#'
 #' @return If `x` is a firmly applied function:
 #'   - `firm_core()` returns a function
 #'   - `firm_error()` returns a character vector
@@ -18,27 +26,18 @@
 #'   `NULL` (in particular, whenever `x` is not of class `firm_closure`).
 #'
 #' @seealso [firmly()]
+#'
 #' @examples
 #' f <- function(x, y, ...) NULL
-#' ff <- firmly(f, is.numeric, {isTRUE(. > 0)} ~ vld(x, y - x))
-#' identical(firm_core(ff), f)  # [1] TRUE
-#' firm_error(ff)               # [1] "inputValidationError"
+#' ff <- firmly(f, is.numeric, {isTRUE(. > 0)}(x, y - x))
+#' identical(firm_core(ff), f)
+#' firm_error(ff)
 #'
-#' @name components
-NULL
-
-firm_closure_extractor <- function(this) {
-  force(this)
-  function(x)
-    if (is_firm(x)) .subset2(environment(x), this) else NULL
-}
-
-#' @rdname components
 #' @export
 firm_core <- firm_closure_extractor("f")
 
-#' @rdname components
 #' @export
+#' @rdname firm_core
 firm_error <- firm_closure_extractor("error_class")
 
 firm_checks <- firm_closure_extractor("chks")
